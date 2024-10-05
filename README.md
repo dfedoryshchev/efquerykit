7c2e9a137fa3872dfba7175e7675c2b8df5b7a67
Dmitriy Fedoryshchev <dmitriy.fedoryshchev@gmail.com>
2024-10-05 12:30:00 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# EfQueryKit
+
+EF Core query extensions for MySQL-backed applications that outgrow naive LINQ.
---
+++ samples/EfQueryKit.Sample/Program.cs
@@ -0,0 +1,2 @@
+// efquerykit - messing about with some ef core query helpers
+Console.WriteLine("efquerykit");
