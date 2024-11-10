1abeea92bfae13e17c49300315725b58ec3a5a54
Dmitriy Fedoryshchev <dmitriy.fedoryshchev@gmail.com>
2024-11-10 15:00:00 +0000

    fulltext - natural language mode too

--- src/EfQueryKit/FullText/FullTextExtensions.cs
+++ src/EfQueryKit/FullText/FullTextExtensions.cs
@@ -2,13 +2,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EfQueryKit.FullText;
 
-// gave up on the linq translation, dropping to raw sql. boolean mode only for now.
+public enum FullTextMode { Boolean, NaturalLanguage }
+
+// raw sql for now. term is parameterized; table/column are identifiers so they go in as-is.
 public static class FullTextExtensions
 {
-    public static IQueryable<T> WhereFullText<T>(this DbSet<T> set, string table, string column, string term)
-        where T : class
+    public static IQueryable<T> WhereFullText<T>(
+        this DbSet<T> set, string table, string column, string term,
+        FullTextMode mode = FullTextMode.Boolean) where T : class
     {
-        var sql = "SELECT * FROM `" + table + "` WHERE MATCH(`" + column + "`) AGAINST ({0} IN BOOLEAN MODE)";
+        var modeSql = mode == FullTextMode.Boolean ? "IN BOOLEAN MODE" : "IN NATURAL LANGUAGE MODE";
+        var sql = "SELECT * FROM `" + table + "` WHERE MATCH(`" + column + "`) AGAINST ({0} " + modeSql + ")";
 #pragma warning disable EF1002
         return set.FromSqlRaw(sql, term);
 #pragma warning restore EF1002
